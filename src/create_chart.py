36646cdab5842c392ad2e395719fa6e53ca14622
cirusthenter <cirusthenter@gmail.com>
2020-10-17 14:29:48 +0900

    modified read_file() in src/create_chart.py

--- src/create_chart.py
+++ src/create_chart.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -12,13 +13,17 @@ def read_file():
     widths = []
     num_nodes = []
     for line in lines:
-        width, num_node = line.split()
+        width, num_node = map(int, line.split())
         widths.append(width)
         num_nodes.append(num_node)
     return widths, num_nodes
 
 
+def draw_chart(x_axis, y_axis):
+    plt.plot(x_axis, y_axis)
+    plt.show()
+
+
 if __name__ == '__main__':
     width_array, num_nodes = read_file()
-    print(width_array)
-    print(num_nodes)
+    draw_chart(width_array, num_nodes)
