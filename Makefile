a6ea20adcb272671e686c4213d24c67f7b480e71
cirusthenter <cirusthenter@gmail.com>
2020-11-08 13:02:54 +0900

    added worst case to integrated chart

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ make:
 	./worst $(addprefix graph/,$(G)) $(W)
 	g++ src/count_degree.cpp -o count_degree --std=c++17
 	./count_degree $(addprefix graph/,$(G))
-	python3 src/create_tree-deg.py $(addprefix output/,$(G)) $(W)
+	python3 src/create_integrated.py $(addprefix output/,$(G)) $(W)
 
 pq:
 	g++ src/min-deg-heu.cpp -o min-deg-heu --std=c++17
--- src/create_integrated.py
+++ src/create_integrated.py
@@ -9,11 +9,11 @@ def read_file():
         exit()
 
     filepath = sys.argv[1]
-    width = sys.argv[2]
+    arg_width = sys.argv[2]
     idx = filepath.find("output/")
     path = filepath[:idx]
     filename = filepath[idx + 7:filepath.find(".txt")]
-    filepath = "output/"+width+"-"+filename+".txt"
+    filepath = "output/"+arg_width+"-"+filename+".txt"
     f = open(filepath, "r")
     lines = f.readlines()
     widths1 = []
@@ -28,7 +28,7 @@ def read_file():
         num_nodes1.append(num_node)
         percentages1.append(num_node/num_nodes * 100)
 
-    filepath = "output/" + width+"-random"+"-"+filename+".txt"
+    filepath = "output/" + arg_width+"-random"+"-"+filename+".txt"
     f = open(filepath, "r")
     lines = f.readlines()
     widths2 = []
@@ -40,29 +40,42 @@ def read_file():
         num_nodes2.append(num_node)
         percentages2.append(num_node/num_nodes * 100)
 
-    filepath = "degree/"+filename+".txt"
+    filepath = "output/" + arg_width+"-worst"+"-"+filename+".txt"
     f = open(filepath, "r")
     lines = f.readlines()
     widths3 = []
     num_nodes3 = []
     percentages3 = []
+    for line in lines:
+        width, num_node = map(int, line.split())
+        widths3.append(width)
+        num_nodes3.append(num_node)
+        percentages3.append(num_node/num_nodes * 100)
+
+    filepath = "degree/"+filename+".txt"
+    f = open(filepath, "r")
+    lines = f.readlines()
+    widths_deg = []
+    num_nodes_deg = []
+    percentages_deg = []
     current_ne = 0
     num_edges = int(lines[0])
     length = len(lines)
     for i in range(1, length):
         line = lines[i]
         deg, num_node = map(int, line.split())
-        widths3.append(deg)
-        num_nodes3.append(num_node)
+        widths_deg.append(deg)
+        num_nodes_deg.append(num_node)
         current_ne += num_node
-        percentages3.append(current_ne/num_nodes * 100)
-    return widths1, percentages1, widths2, percentages2, widths3, percentages3, path, filename
+        percentages_deg.append(current_ne/num_nodes * 100)
+    return widths1, percentages1, widths2, percentages2, widths3, percentages3, widths_deg, percentages_deg, path, filename
 
 
-def draw_chart(x_axis1, y_axis1, x_axis2, y_axis2, x_axis3, y_axis3, path, filename):
+def draw_chart(x_axis1, y_axis1, x_axis2, y_axis2, x_axis3, y_axis3, x_axis4, y_axis4, path, filename):
     plt.plot(x_axis1, y_axis1,  c="b",  label="optimal")
-    plt.plot(x_axis2, y_axis2, c="r",  label="random")
-    plt.plot(x_axis3, y_axis3, c="g",  label="degree")
+    plt.plot(x_axis2, y_axis2, c="g",  label="random")
+    plt.plot(x_axis3, y_axis3, c="r",  label="worst")
+    plt.plot(x_axis4, y_axis4, c="k",  label="degree")
     width = sys.argv[2]
     saved_name = path + "charts/integrated-" + width + "-" + filename + ".pdf"
     print(saved_name)
@@ -76,6 +89,6 @@ def draw_chart(x_axis1, y_axis1, x_axis2, y_axis2, x_axis3, y_axis3, path, filen
 
 
 if __name__ == '__main__':
-    widths1, percentages1, widths2, percentages2, widths3, percentages3, path, filename = read_file()
-    draw_chart(widths1, percentages1, widths2, percentages2,
-               widths3, percentages3, path, filename)
+    widths1, percentages1, widths2, percentages2, widths3, percentages3, widths_deg, percentages_deg, path, filename = read_file()
+    draw_chart(widths1, percentages1, widths2, percentages2, widths3,
+               percentages3, widths_deg, percentages_deg, path, filename)
