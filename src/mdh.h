37c5c8a361a3debfea010a77da084777d37ecc3f
cirusthenter <cirusthenter@gmail.com>
2020-12-17 19:30:21 +0900

    checkpoint before git pull

--- src/mdh.h
+++ src/mdh.h
@@ -3,6 +3,7 @@
 class mdh : public Graph {
 public:
     unordered_map<int, unordered_set<int>> neighbors_of;
+    int init_degree;
 
     void make_graph()
     {
@@ -21,6 +22,17 @@ public:
                 if (neighbors_of[nbr1].find(nbr2) == neighbors_of[nbr1].end())
                     neighbors_of[nbr1].insert(nbr2);
             }
+        }
+    }
+
+    int random_node()
+    {
+        while (true) {
+            int index = rand();
+            index %= num_nodes;
+            init_degree = neighbors_of[index].size();
+            if (init_degree > 0)
+                return index;
         }
     }
 };
