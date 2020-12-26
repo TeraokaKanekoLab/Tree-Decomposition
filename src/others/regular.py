f23d92a07f553c5f30c3cc662a212d0e6f0494f3
Cirus Thenter <cirusthenter@gmail.com>
2020-12-27 01:51:00 +0900

    regular graph

--- src/others/regular.py
+++ src/others/regular.py
@@ -1,11 +1,13 @@
 import networkx as nx
+from networkx.classes.function import number_of_nodes
 
 degree = 3
+num_nodes = 1000000
 
-filename = "graph/" + str(degree) + "-regular.gr"
+filename = "graph/" + str(degree) + "-regular-" + str(num_nodes) + ".gr"
 print(filename)
 f = open(filename, "w")
-G = nx.generators.random_graphs.random_regular_graph(degree, 1000000)
+G = nx.generators.random_graphs.random_regular_graph(degree, num_nodes)
 for edge in G.edges:
     e = str(edge[0]) + " " + str(edge[1]) + "\n"
     f.write(e)
