82a7226e0eebf81988b0c2b5c0c6158644800bf3
Cirus Thenter <cirusthenter@gmail.com>
2021-01-21 10:16:38 +0900

    log

--- src/evaluate/betweenness_centrality.py
+++ src/evaluate/betweenness_centrality.py
@@ -2,6 +2,7 @@ import networkx as nx
 from networkx.algorithms.centrality import betweenness_centrality
 import sys
 import time
+from datetime import datetime
 
 
 def read_graph():
@@ -28,15 +29,32 @@ def write_to_file(bcs):
     for key in bcs:
         line = str(key) + " " + str(bcs[key]) + "\n"
         f.write(line)
+    f.close()
+
+
+def write_log(filepath, log):
+    f = open(filepath, "a")
+    f.write(log)
+    f.close()
 
 
 if __name__ == "__main__":
+    start_datetime = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+    filepath = "log/" + start_datetime + ".log"
+    msg = "src/evaluate/betweenness_centrality.py " + sys.argv[1] + "\n"
+    write_log(filepath, msg)
     print("loading...")
     graph = read_graph()
     print("calculating...")
     start = time.time()
+    msg = "program execution started at " + start_datetime + "\n"
+    write_log(filepath, msg)
     bcs = betweenness_centrality(graph)
     end = time.time()
     print("writing result...")
     write_to_file(bcs)
-    write_to_file(end-start)
+    end_datetime = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+    msg = "program execution ended at " + end_datetime + "\n"
+    write_log(filepath, msg)
+    msg = "total execution time: " + str(end-start) + "\n"
+    write_log(filepath, msg)
